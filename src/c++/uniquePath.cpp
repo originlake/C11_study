37ced838e2933acfbe44cdd03efafed71f839763
s.zhong <11423220+originlake@users.noreply.github.com>
2018-04-08 19:30:46 -0500

    Update uniquePath.cpp

--- src/c++/uniquePath.cpp
+++ src/c++/uniquePath.cpp
@@ -30,4 +30,18 @@ public:
 		}
 		return a[n-1];
 	}
+	int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+		if (grid.empty() || grid[0].empty()) return 0;
+		if (grid[0][0] == 1) return 0;
+		int m= grid.size(),n = grid[0].size();
+		vector<int> a(n,0);
+		a[0] = 1;
+		for (int i = 0; i < m; i++) {
+			a[0] = (a[0] == 0 || grid[i][0] == 1) ? 0 : 1;
+			for (int j = 1; j < n; j++) {
+				a[j] = grid[i][j] == 1 ? 0 : (a[j] + a[j - 1]);
+			}
+		}
+		return a[n - 1];
+	}
 };
