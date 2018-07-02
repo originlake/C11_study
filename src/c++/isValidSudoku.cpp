bf7a01eb9f5e7811a9cdfa6cb24f9c9704446d24
s.zhong <11423220+originlake@users.noreply.github.com>
2018-07-02 14:02:42 +0800

    Update isValidSudoku.cpp

--- src/c++/isValidSudoku.cpp
+++ src/c++/isValidSudoku.cpp
@@ -18,8 +18,11 @@ public:
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				if (board[i][j] != '.') {
+					//row
 					s[0].push_back(board[i][j]); s[0].push_back('+'); s[0].push_back('0' + i);
+					//column
 					s[1].push_back(board[i][j]); s[1].push_back('-'); s[1].push_back('0' + j);
+					//3 by 3 block
 					s[2].push_back(board[i][j]); s[2].push_back('*'); s[2].push_back('0' + (i / 3 * 3 + j / 3));
 					if (check[s[0]] || check[s[1]] || check[s[2]]) {
 						return false;
